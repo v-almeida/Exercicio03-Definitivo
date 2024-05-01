af677781c74e4297559e1517b05be6a1569f73d7
vitor <almeida.vitor20000@gmail.com>
2024-04-30 23:46:44 -0300

    décimo terceiro exercicio

---
+++ src/br/edu/up/Exercicio23.java
@@ -0,0 +1,32 @@
+package br.edu.up;
+
+import java.util.Scanner;
+
+import br.edu.up.modelos.Mmc23;
+
+public class Exercicio23 {
+
+    public static void Executar() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite o nome da pessoa:");
+        String nome = scanner.nextLine();
+
+        System.out.println("Digite o sexo da pessoa (M/F):");
+        char sexo = scanner.next().charAt(0);
+
+        System.out.println("Digite a altura da pessoa:");
+        double altura = scanner.nextDouble();
+
+        System.out.println("Digite a idade da pessoa:");
+        int idade = scanner.nextInt();
+
+        Mmc23 pessoa = new Mmc23(nome, sexo, altura, idade);
+        double pesoIdeal = pessoa.calcularPesoIdeal();
+
+        System.out.println("Nome: " + nome);
+        System.out.println("Peso Ideal: " + pesoIdeal + " kg");
+
+        scanner.close();
+    }
+}
