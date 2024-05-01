181a306122ec99f5ebdd1dd03afa0c23cca4c221
vitor <almeida.vitor20000@gmail.com>
2024-04-30 23:43:44 -0300

    décimo primeiro exercicio

---
+++ src/br/edu/up/Exercicio21.java
@@ -0,0 +1,21 @@
+package br.edu.up;
+
+import java.util.Scanner;
+
+import br.edu.up.modelos.Nadador21;
+
+public class Exercicio21 {
+    public static void Executar() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite a idade do nadador:");
+        int idade = scanner.nextInt();
+
+        Nadador21 nadador = new Nadador21(idade);
+        String categoria = nadador.classificarNadador();
+
+        System.out.println("O nadador está na categoria: " + categoria);
+
+        scanner.close();
+    }
+}
---
+++ src/br/edu/up/modelos/Nadador21.java
@@ -0,0 +1,33 @@
+package br.edu.up.modelos;
+
+public class Nadador21 {
+    private int idade;
+
+    public Nadador21(int idade) {
+        this.idade = idade;
+    }
+
+    public String classificarNadador() {
+        if (idade >= 5 && idade <= 7) {
+            return "Infantil A";
+        } else if (idade >= 8 && idade <= 10) {
+            return "Infantil B";
+        } else if (idade >= 11 && idade <= 13) {
+            return "Juvenil A";
+        } else if (idade >= 14 && idade <= 17) {
+            return "Juvenil B";
+        } else if (idade >= 18 && idade <= 25) {
+            return "Sênior";
+        } else {
+            return "Idade fora da faixa etária";
+        }
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+}
