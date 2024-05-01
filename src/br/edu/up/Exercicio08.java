6a0e1a3eef87ab43b11748029c95bc9413fc1a72
vitor <almeida.vitor20000@gmail.com>
2024-04-30 23:21:49 -0300

    oitavo exercicio

---
+++ src/br/edu/up/Exercicio08.java
@@ -0,0 +1,33 @@
+package br.edu.up;
+
+import java.util.Scanner;
+
+import br.edu.up.modelos.Estudante;
+
+public class Exercicio08 {
+    public static void Executar() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite o nome do estudante: ");
+        String nome = scanner.nextLine();
+
+        System.out.print("Digite a primeira nota: ");
+        double nota1 = scanner.nextDouble();
+
+        System.out.print("Digite a segunda nota: ");
+        double nota2 = scanner.nextDouble();
+
+        System.out.print("Digite a terceira nota: ");
+        double nota3 = scanner.nextDouble();
+
+        Estudante estudante = new Estudante(nome, nota1, nota2, nota3);
+        double media = estudante.calcularMedia();
+        String mencao = estudante.obterMencao();
+
+        System.out.println("Nome do estudante: " + estudante.getNome());
+        System.out.println("Média: " + media);
+        System.out.println("Menção: " + mencao);
+
+        scanner.close();
+    }
+}
---
+++ src/br/edu/up/modelos/Estudante.java
@@ -0,0 +1,58 @@
+package br.edu.up.modelos;
+
+public class Estudante {
+    private String nome;
+    private double nota1;
+    private double nota2;
+    private double nota3;
+
+    public Estudante(String nome, double nota1, double nota2, double nota3) {
+        this.nome = nome;
+        this.nota1 = nota1;
+        this.nota2 = nota2;
+        this.nota3 = nota3;
+    }
+
+    public double calcularMedia() {
+        return (nota1 + nota2 + nota3) / 3;
+    }
+
+    public String obterMencao() {
+        double media = calcularMedia();
+        if (media >= 7.0) {
+            return "Aprovado";
+        } else if (media <= 5.0) {
+            return "Reprovado";
+        } else {
+            return "Recuperação";
+        }
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public double getNota1() {
+        return nota1;
+    }
+
+    public double getNota2() {
+        return nota2;
+    }
+
+    public double getNota3() {
+        return nota3;
+    }
+
+    public void setNota1(double nota1) {
+        this.nota1 = nota1;
+    }
+
+    public void setNota2(double nota2) {
+        this.nota2 = nota2;
+    }
+
+    public void setNota3(double nota3) {
+        this.nota3 = nota3;
+    }
+}
