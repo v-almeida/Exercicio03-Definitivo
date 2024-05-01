89f48e53e670ad70b73446baeba070ed182fa319
vitor <almeida.vitor20000@gmail.com>
2024-04-30 23:33:47 -0300

    décimo quarto exercicio

---
+++ src/br/edu/up/Exercicio14.java
@@ -0,0 +1,44 @@
+package br.edu.up;
+
+import java.util.Scanner;
+import br.edu.up.modelos.Produto;
+
+public class Exercicio14 {
+    public static void Executar() {
+        Scanner scanner = new Scanner(System.in);
+
+        Produto[] produtos = new Produto[4];
+        double somaPrecoCusto = 0;
+        double somaPrecoVenda = 0;
+
+        // Recebendo os dados dos produtos
+        for (int i = 0; i < produtos.length; i++) {
+            System.out.println("Informe o preço de custo do produto " + (i + 1) + ":");
+            double precoCusto = scanner.nextDouble();
+
+            System.out.println("Informe o preço de venda do produto " + (i + 1) + ":");
+            double precoVenda = scanner.nextDouble();
+
+            produtos[i] = new Produto(precoCusto, precoVenda);
+            somaPrecoCusto += precoCusto;
+            somaPrecoVenda += precoVenda;
+        }
+
+        // Calculando médias
+        double mediaPrecoCusto = somaPrecoCusto / produtos.length;
+        double mediaPrecoVenda = somaPrecoVenda / produtos.length;
+
+        System.out.println("\nResultados:");
+
+        // Mostrando se houve lucro, prejuízo ou empate para cada produto
+        for (int i = 0; i < produtos.length; i++) {
+            System.out.println("Produto " + (i + 1) + ": " + produtos[i].verificarLucroOuPrejuizo());
+        }
+
+        // Mostrando média de preço de custo e preço de venda
+        System.out.println("\nMédia de preço de custo: " + mediaPrecoCusto);
+        System.out.println("Média de preço de venda: " + mediaPrecoVenda);
+
+        scanner.close();
+    }
+}
---
+++ src/br/edu/up/modelos/Produto.java
@@ -0,0 +1,37 @@
+package br.edu.up.modelos;
+
+public class Produto {
+    private double precoCusto;
+    private double precoVenda;
+
+    public Produto(double precoCusto, double precoVenda) {
+        this.precoCusto = precoCusto;
+        this.precoVenda = precoVenda;
+    }
+
+    public double getPrecoCusto() {
+        return precoCusto;
+    }
+
+    public void setPrecoCusto(double precoCusto) {
+        this.precoCusto = precoCusto;
+    }
+
+    public double getPrecoVenda() {
+        return precoVenda;
+    }
+
+    public void setPrecoVenda(double precoVenda) {
+        this.precoVenda = precoVenda;
+    }
+
+    public String verificarLucroOuPrejuizo() {
+        if (precoVenda > precoCusto) {
+            return "Lucro";
+        } else if (precoVenda < precoCusto) {
+            return "Prejuízo";
+        } else {
+            return "Empate";
+        }
+    }
+}
