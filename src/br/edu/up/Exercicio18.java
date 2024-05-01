a210c92b9b3c496f5f365d893c1bdaa0449cbab1
vitor <almeida.vitor20000@gmail.com>
2024-04-30 23:39:56 -0300

    décimo oitavo exercicio

---
+++ src/br/edu/up/Exercicio18.java
@@ -0,0 +1,31 @@
+package br.edu.up;
+
+import java.util.Scanner;
+
+import br.edu.up.modelos.Trampo18;
+
+public class Exercicio18 {
+    public static void Executar() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Informe o nome do funcionário:");
+        String nome = scanner.nextLine();
+
+        System.out.println("Informe a idade do funcionário:");
+        int idade = scanner.nextInt();
+
+        System.out.println("Informe o sexo do funcionário (M/F):");
+        char sexo = scanner.next().charAt(0);
+
+        System.out.println("Informe o salário fixo do funcionário:");
+        double salarioFixo = scanner.nextDouble();
+
+        Trampo18 funcionario = new Trampo18(nome, idade, sexo, salarioFixo);
+        double salarioLiquido = funcionario.calcularSalarioLiquido();
+
+        System.out.println("\nNome: " + nome);
+        System.out.println("Salário Líquido: R$" + salarioLiquido);
+
+        scanner.close();
+    }
+}
---
+++ src/br/edu/up/modelos/Trampo18.java
@@ -0,0 +1,62 @@
+package br.edu.up.modelos;
+
+public class Trampo18 {
+    private String nome;
+    private int idade;
+    private char sexo;
+    private double salarioFixo;
+
+    public Trampo18(String nome, int idade, char sexo, double salarioFixo) {
+        this.nome = nome;
+        this.idade = idade;
+        this.sexo = sexo;
+        this.salarioFixo = salarioFixo;
+    }
+
+    public double calcularSalarioLiquido() {
+        double abono;
+        if (sexo == 'M' && idade >= 30) {
+            abono = 100.00;
+        } else if (sexo == 'M' && idade < 30) {
+            abono = 50.00;
+        } else if (sexo == 'F' && idade >= 30) {
+            abono = 200.00;
+        } else {
+            abono = 80.00;
+        }
+
+        return salarioFixo + abono;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public char getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(char sexo) {
+        this.sexo = sexo;
+    }
+
+    public double getSalarioFixo() {
+        return salarioFixo;
+    }
+
+    public void setSalarioFixo(double salarioFixo) {
+        this.salarioFixo = salarioFixo;
+    }
+}
